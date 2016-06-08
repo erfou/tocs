8a5884da5f557ebb88a42bdae9a0e016cf4ff2e2
Eric <eric.fournier.06@gmail.com>
2016-06-08 06:06:35 +0000

    Seat map security view implementation

--- server-side/rest-api/pnc/seatMapManager.js
+++ server-side/rest-api/pnc/seatMapManager.js
@@ -7,9 +7,33 @@ var SeatMapManager = {
     	var serviceSeatMapView = new ServiceSeatMapView();
         SeatService.getAllSeats(function(err, allSeats) {
             if(!err) {
-            	for(var seat of allSeats.seats) {
-            		serviceSeatMapView.seatMapView.push(new SeatInfosView(seat));
-            	}            	
+                var listOfMissingPassengers = [], nbSeatsFree = 0, nbPassengers = 0, nbSeatsOccuped = 0;
+                if('security' == typeOfView) {
+                    for(var seat of allSeats.seats) {
+                        if(seat.currentPassenger) {
+                            nbPassengers++;
+                        }
+
+                        if(seat.occuped === false ) {
+                            nbSeatsFree++;
+                            if(seat.currentPassenger) {
+                                listOfMissingPassengers.push(seat.currentPassenger);
+                            }
+                        } else {
+                            nbSeatsOccuped++;                            
+                        }
+                        serviceSeatMapView.seatMapView.push(new SeatInfosView(seat));
+                    }
+                    
+                    serviceSeatMapView.securityView = {};
+                    if(listOfMissingPassengers.length) {
+                        serviceSeatMapView.securityView.listOfMissingPassengersView = listOfMissingPassengers;
+                    }
+
+                    serviceSeatMapView.securityView.nbSeatsFree = nbSeatsFree;
+                    serviceSeatMapView.securityView.nbPassengers = nbPassengers;
+                    serviceSeatMapView.securityView.nbSeatsOccuped = nbSeatsOccuped;
+                }
                 callback(null, serviceSeatMapView);
             } else {
                 callback(err, null);
