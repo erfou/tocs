39a1a19b853a70e0f557c82276d5f3a112dca30d
fournier <eric.fournier.06@gmail.com>
2016-06-10 08:15:02 +0200

    Add nb missing passenger in security view

--- server-side/rest-api/pnc/seatMapManager.js
+++ server-side/rest-api/pnc/seatMapManager.js
@@ -7,7 +7,7 @@ var SeatMapManager = {
     	var serviceSeatMapView = new ServiceSeatMapView();
         SeatService.getAllSeats(function(err, allSeats) {
             if(!err) {
-                var listOfMissingPassengers = [], nbSeatsFree = 0, nbPassengers = 0, nbSeatsOccuped = 0;
+                var listOfKnowMissingPassengersView = [], nbSeatsFree = 0, nbPassengers = 0, nbSeatsOccuped = 0;
                 if('security' == typeOfView) {
                     for(var seat of allSeats.seats) {
                         if(seat.currentPassenger) {
@@ -17,7 +17,7 @@ var SeatMapManager = {
                         if(seat.occuped === false ) {
                             nbSeatsFree++;
                             if(seat.currentPassenger) {
-                                listOfMissingPassengers.push(seat.currentPassenger);
+                                listOfKnowMissingPassengersView.push(seat.currentPassenger);
                             }
                         } else {
                             nbSeatsOccuped++;                            
@@ -26,12 +26,13 @@ var SeatMapManager = {
                     }
                     
                     serviceSeatMapView.securityView = {};
-                    if(listOfMissingPassengers.length) {
-                        serviceSeatMapView.securityView.listOfMissingPassengersView = listOfMissingPassengers;
+                    if(listOfKnowMissingPassengersView.length > 0) {
+                        serviceSeatMapView.securityView.listOfKnowMissingPassengersView = listOfKnowMissingPassengersView;
                     }
 
-                    serviceSeatMapView.securityView.nbSeatsFree = nbSeatsFree;
                     serviceSeatMapView.securityView.nbPassengers = nbPassengers;
+                    serviceSeatMapView.securityView.nbMissingPassengers = nbPassengers - nbSeatsOccuped;
+                    serviceSeatMapView.securityView.nbSeatsFree = nbSeatsFree;
                     serviceSeatMapView.securityView.nbSeatsOccuped = nbSeatsOccuped;
                 }
                 callback(null, serviceSeatMapView);
