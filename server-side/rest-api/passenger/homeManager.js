9b70f942b092e988a2dfbbb12c5e4fbd90e9b4c8
Eric <eric.fournier.06@gmail.com>
2016-06-28 09:12:48 +0000

    retrieve seat in Tokenizater instead of in homManager.

--- server-side/rest-api/passenger/homeManager.js
+++ server-side/rest-api/passenger/homeManager.js
@@ -2,32 +2,14 @@ var async = require('async');
 /*TODO : check what is required here*/
 var crudApi = require('app_modules/crud-api');
 var Tokenizer = require('./tokenizer');
+var HomeView = require('./homeView');
 
 var CategoryService = crudApi.categories.services;
-var seatService = crudApi.seats.services;
 var CategoryView = crudApi.categories.view;
-var SeatView = crudApi.seats.view;
 
 var homeManager = {
     
     load : function(req, callback) {
-        var homeView = {
-            token: req.body.token,
-    		breadcrumbElements: [
-    			{
-    				link: {
-    					rel: "Acceuil",
-    					href: "/passenger/home"
-    				},
-    				current: true
-    			}
-    		],
-            seatView: {},
-            categoriesView : {
-			    title: "Services disponibles",
-                categories: []
-            }
-        };
         
         async.waterfall([
             function(callback) { 
@@ -38,20 +20,6 @@ var homeManager = {
                         callback(err, null);
                     }
                 });
-            },
-            function(passenger, callback) {
-              if(!passenger.seat._id) {
-                  seatService.getSeatById(passenger.seat, function(err, result){
-                      if(!err) {
-                          passenger.seat = result;
-                          callback(null, passenger);
-                      } else {
-                          
-                      }
-                  });
-              } else {
-                  callback(null, passenger);
-              }
             },
             function(passenger, callback) {
                 CategoryService.getCategoriesByFareClass(passenger.seat.fareClass, function(err, result) {
@@ -66,7 +34,7 @@ var homeManager = {
         ],
         function(err, seat, result) {
             if(!err) {
-                homeView.seatView = new SeatView(seat);
+                var homeView = new HomeView(seat, 0);
                 if(result.categories) {
             		for (var category of result.categories) {
             			homeView.categoriesView.categories.push(new CategoryView(category));
--- server-side/rest-api/passenger/tokenizer.js
+++ server-side/rest-api/passenger/tokenizer.js
@@ -1,3 +1,6 @@
+var async = require('async');
+var seatService = require('app_modules/crud-api').seats.services;
+
 var Tokenizer = {
     tokenize : function (passenger, callback) {
 
@@ -10,7 +13,31 @@ var Tokenizer = {
         callback(null, new Buffer(JSON.stringify(passenger)).toString('base64'));
     },
     detokenize : function (token, callback) {
-        callback(null, JSON.parse(new Buffer(token, 'base64').toString('ascii')));
+        async.waterfall([
+           function(callback) {
+               callback(null, JSON.parse(new Buffer(token, 'base64').toString('ascii')));
+           },
+           function(passenger, callback) {
+               if(!passenger.seat._id) {
+                    seatService.getSeatById(passenger.seat, function(err, result){
+                        if(!err) {
+                            passenger.seat = result;
+                            callback(null, passenger);
+                        } else {
+                            callback(err, null);
+                        }
+                    });
+               } else {
+                   callback(null, passenger);
+               }
+           }
+        ], function(err, passenger) {
+            if(!err) {
+                callback(null, passenger);
+            } else {
+                callback(err, null);
+            }
+        });
     }
 };
 
